67b80881fd67df13de7f100aac080c8021807e92
Chenyinan <18322913588@163.com>
2019-10-11 16:30:11 +0800

    Initial commit

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.1.173', // can be overwritten by process.env.HOST
+    host: '120.24.160.251', // can be overwritten by process.env.HOST
     port: 82, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
